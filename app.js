9a81e33724f72498d07ad8995da46da3151c8b89
hackothaurus <hakhan.bscs17seecs@seecs.edu.pk>
2020-07-24 14:42:10 +0500

    Basic structure including angularjs, with its functionality used to bind view index.html with modelview app.js. App.js contains angular module, the app, and angular controller, the functionality of the view. :)

---
+++ app.js
@@ -0,0 +1,12 @@
+(function(){ //EEFI
+    'use strict';
+    //this automatic binding is being done by AngularJS bw View and ViewModel
+
+    //binding of the app and the controller
+angular.module('myFirstapp', [])
+
+.controller('MyFirstController', function(){
+
+});
+
+})();
